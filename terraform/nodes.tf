1992d1c9e2a416df09e2f80dcec20336247bf48c
Michael Monroe <michael.monroe@servian.com>
2020-04-21 18:51:12 +1000

    Update nodes.tf to allow for security patched version of Ubuntu

--- terraform/nodes.tf
+++ terraform/nodes.tf
@@ -61,7 +61,7 @@ resource "azurerm_virtual_machine" "rke" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "16.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
 
