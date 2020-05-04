8ac06696d58875278aa447c228de2ecb4d6f15c6
Mark Revill <mrevill@servian.com>
2020-05-04 14:48:58 +1000

    Added TF version pin and validate

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -12,7 +12,7 @@ resource "azurerm_virtual_network" "network" {
 
 # Create subnets
 resource "azurerm_subnet" "subnet" {
-  name                            = "subnet-rke"
+  name                 = "subnet-rke"
   resource_group_name  = azurerm_resource_group.resourcegroup.name
   virtual_network_name = azurerm_virtual_network.network.name
   address_prefix       = "10.0.1.0/24"
